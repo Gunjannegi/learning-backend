8d3d158977beef36cb212259d7d9cca1fce04a73
Gunjannegi <gunjan.negi.5249@gmail.com>
2025-09-07 16:28:34 +0530

    corrected mistakes

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const app = express();
 const port = 3000;      
 
 app.use("/welcome",(req, res, next) => {
-    const modifiedReq = { ...req, "user":"Guest"}
-    console.log(modifiedReq.user);
-    res.send(`<h1>Welcome ${modifiedReq.user}</h1>`);
-    next();
+    req.user = "Guest";
+    res.send(`<h1>Welcome ${req.user}</h1>`);
 });
 app.listen(port, ()=>{
     console.log("Server is up and running on port 3000! Ready to handle requests.")
