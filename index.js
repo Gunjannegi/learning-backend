452de9679ee545d5703031f8aa65e8d2a55aa1e4
Gunjannegi <gunjan.negi.5249@gmail.com>
2025-09-07 16:25:07 +0530

    Middleware and Routing

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;      
 
+app.use("/welcome",(req, res, next) => {
+    const modifiedReq = { ...req, "user":"Guest"}
+    console.log(modifiedReq.user);
+    res.send(`<h1>Welcome ${modifiedReq.user}</h1>`);
+    next();
+});
 app.listen(port, ()=>{
     console.log("Server is up and running on port 3000! Ready to handle requests.")
 })
