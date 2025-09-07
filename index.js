1f99743f3588b66830cf9ca63811b4179e2e906e
Gunjannegi <gunjan.negi.5249@gmail.com>
2025-09-07 17:56:00 +0530

    Routes

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;      
 
-app.use("/welcome",(req, res, next) => {
-    req.user = "Guest";
-    res.send(`<h1>Welcome ${req.user}</h1>`);
+app.get("/", (req, res)=>{
+    res.send("Here is the list of all orders.");
 });
+
+app.post("/orders", (req, res)=>{
+    res.send("Order created successfully.");
+});
+
+app.get("/users", (req, res)=>{
+    res.send("Here is the list of all users.");
+});
+
+app.post("/users", (req, res)=>{
+    res.send("A new user has been added.");
+});
+
 app.listen(port, ()=>{
-    console.log("Server is up and running on port 3000! Ready to handle requests.")
+    console.log( "Server is running on http://localhost:3000")
 })
