6cd16d8dd204addaef8fc10325b1a32c8ff32b3d
Gunjannegi <gunjan.negi.5249@gmail.com>
2025-09-09 05:10:09 +0530

    Unknown route

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const app = express();
-const port = 3000;      
+const port = 4000;
 
-app.get("/", (req, res)=>{
-    res.send("Here is the list of all orders.");
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+    res.send("Here is the list of all products.");
 });
 
-app.post("/orders", (req, res)=>{
-    res.send("Order created successfully.");
+app.post("/products", (req, res) => {
+    res.send("A new product has been added.");
 });
 
-app.get("/users", (req, res)=>{
-    res.send("Here is the list of all users.");
+app.get("/categories", (req, res) => {
+    res.send("Here is the list of all categories.");
 });
 
-app.post("/users", (req, res)=>{
-    res.send("A new user has been added.");
+app.post("/categories", (req, res) => {
+    res.send("A new category has been created.");
 });
 
-app.listen(port, ()=>{
-    console.log( "Server is running on http://localhost:3000")
-})
+app.use((req, res) => {
+    res.status(404).type('html').send("<h1>404 - Page Not Found</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
